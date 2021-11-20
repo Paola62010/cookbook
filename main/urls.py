66e956724e627a2513a2c2c5bee831a24bee6a69
Paola <paola62010@gmail.com>
2021-11-20 17:24:58 +0000

    Add templates, views and urls for public and personal recipe list

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.CategoryList.as_view(), name='home')
+    path('', views.CategoryList.as_view(), name='home'),
+    path('public', views.PublicRecipeList.as_view(), name='public_recipes'),
+    path('personal', views.PersonalRecipeList.as_view(), name='personal_recipes'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Category
+from .models import Category, Recipe
 
 
 class CategoryList(generic.ListView):
     model = Category
     queryset = Category.objects.all()
     template_name = 'index.html'
+
+
+class PublicRecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(public=True).order_by('-created_on')
+    template_name = 'public_recipes.html'
+
+
+class PersonalRecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(public=False).order_by('-created_on')
+    template_name = 'personal_recipes.html'
